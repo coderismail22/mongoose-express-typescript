1ad2c44ee897d1d2247075bdfb6bf9620388f4d1
ismail <coderismail22@gmail.com>
2024-05-17 13:59:29 +0600

    tweak-linting

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-import mongoose from "mongoose";
-import config from "./app/config";
-import app from "./app";
+import mongoose from 'mongoose';
+import config from './app/config';
+import app from './app';
 
 async function main() {
-
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
