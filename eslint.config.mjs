4eb62ef3bec0acf6a1bbbd461b59bc96f6fa36b9
ismail <coderismail22@gmail.com>
2024-05-17 14:24:09 +0600

    add-prettier-script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'prettier-eslint';
+
 export default [
-{
-ignores: ["**/node_modules/", ".dist/"],
-languageOptions: {
-globals: {
-...globals.browser,
-process: "readonly",
-},
-},
-rules: {
-"no-unused-vars": "error",
-"no-unused-expressions": "error",
-"prefer-const": "error",
-"no-console": "warn",
-"no-undef": "error",
-},
-},
-pluginJs.configs.recommended,
-...tseslint.configs.recommended,
+  {
+    ignores: ['**/node_modules/', '.dist/'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+      ...prettier.rules,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
