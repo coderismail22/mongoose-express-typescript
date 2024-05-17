bddb014ffbc8ed6f5cd47733790b35bfd8e01ff3
ismail <coderismail22@gmail.com>
2024-05-17 15:21:19 +0600

    create-student-interface

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,40 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type Username = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+export type Student = {
+  id: string;
+  name: Username;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  avatar?: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'inactive';
+};
