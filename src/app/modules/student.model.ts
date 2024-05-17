2a1686c69b36eb7f97a07f2feeed2cc07a446203
ismail <coderismail22@gmail.com>
2024-05-17 16:17:39 +0600

    refactor-schema

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -1,23 +1,67 @@
 import { Schema } from 'mongoose';
-import { Student } from './student/student.interface';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  Username,
+} from './student/student.interface';
+
+
+//Schema Creation:
 
 //You have to write types in capital letters in mongoose.
-const userSchema = new Schema<Student>({
-  id: String,
-  name: {
-    firstName: {
-      type: String,
-      required: true,
-    },
-    middleName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
+const userNameSchema = new Schema<Username>({
+  firstName: {
+    type: String,
+    required: true,
   },
+  middleName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  motherOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const studentSchema = new Schema<Student>({
+  id: String,
+  name: userNameSchema,
   gender: ['male', 'female'], // enum
   dateOfBirth: { type: String, required: true },
   email: { type: String, required: true },
@@ -26,36 +70,6 @@ const userSchema = new Schema<Student>({
   bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
-  guardian: {
-    fatherName: {
-      type: String,
-      required: true,
-    },
-    fatherOccupation: {
-      type: String,
-      required: true,
-    },
-    fatherContactNo: {
-      type: String,
-      required: true,
-    },
-    motherName: {
-      type: String,
-      required: true,
-    },
-    motherOccupation: {
-      type: String,
-      required: true,
-    },
-    motherContactNo: {
-      type: String,
-      required: true,
-    },
-  },
-  localGuardian:{
-   name:{type:String,required:true},
-   occupation:{type:String,required:true},
-   contactNo:{type:String,required:true},
-   address:{type:String,required:true}
-  }
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
 });
