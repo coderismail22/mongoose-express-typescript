f4134fed7b7562297bb68d945cd7a0ff2901ad2c
ismail <coderismail22@gmail.com>
2024-05-25 21:16:06 +0600

    refactor router

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,16 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/route.student';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFoundHandler from './app/middlewares/notFoundHandler';
+import router from './app/routes';
 const app: Application = express();
-// const port = 3000;
 
 //parser
 app.use(express.json()); // json parser
 app.use(cors());
 
 //application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1/', router);
 
 //middlewares
 app.use(globalErrorHandler);
