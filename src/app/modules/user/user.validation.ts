0ed5700e81f9d36ba108a2c88ef4bb1377c7f1b4
ismail <coderismail22@gmail.com>
2024-05-25 12:27:30 +0600

    refactor user validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,12 +1,16 @@
 import { z } from 'zod';
 
 const userValidationSchema = z.object({
-  pasword: z
+  id: z.string(),
+  password: z
     .string({
       invalid_type_error: 'Password must be string',
     })
     .max(20, { message: 'Password can not be more than 20 characters' })
     .optional(),
+  needsPasswordChange: z.boolean().optional().default(true),
+  role: z.enum(['in-progress', 'blocked']).default('in-progress'),
+  isDeleted: z.boolean().optional().default(false),
 });
 
 export const UserValidation = {
