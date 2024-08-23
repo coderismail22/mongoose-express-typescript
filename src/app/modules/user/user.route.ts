628d36a6f4bf06d937f8c919b04a3d006e7f86de
ismail <coderismail22@gmail.com>
2024-08-23 12:13:16 +0600

    update userValidation Schema for zod

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -12,3 +12,4 @@ router.post(
 );
 
 export const UserRoutes = router;
+ 
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,14 +1,14 @@
 import { z } from 'zod';
 
 const userValidationSchema = z.object({
-  id: z.string(),
+  id: z.string({
+    message: 'Id is a must.',
+    invalid_type_error: 'id should must be a string',
+  }),
   password: z
     .string()
-    .max(20, { message: 'Password cannot be less than 20 characters' }),
-  needsPasswordChange: z.boolean().optional().default(true),
-  role: z.enum(['student', 'faculty', 'admin']),
-  status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-  isDeleted: z.boolean().optional().default(false),
+    .max(20, { message: 'Password cannot be less than 20 characters' })
+    .optional(),
 });
 
 export const UserValidation = {
